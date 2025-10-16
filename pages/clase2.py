e87b461c89422c9e8a693be9c3bb2ae29f2db66b
Luis <luisarmando142003@gmail.com>
2025-10-15 23:29:26 -0500

    clase 2

---
+++ pages/clase2.py
@@ -0,0 +1,68 @@
+import dash
+from dash import html, dcc, Input, Output, State, callback
+import numpy as np
+import plotly.graph_objects as go
+from utils.funciones import func_graficar_ecu_log
+
+dash.register_page(__name__, path='/pagina3', name='Página 3')
+
+
+## Layout de la página ##
+
+layout = html.Div(children=[
+    #contenedor izquierdo
+    html.Div([
+        html.H2("Parámetros del Modelo", className="title"),
+
+        html.Div([
+            html.Label("Población Inicial P(0):"),
+            dcc.Input(id="input-p0", type="number", value=200, className="input-field"),
+        ], className="input-group"),
+
+        html.Div([
+            html.Label("Tasa de Crecimiento (r):"),
+            dcc.Input(id="input-r", type="number", value=0.04, step=0.01 ,className="input-field"),
+        ], className="input-group"),
+
+        html.Div([
+            html.Label("Capacidad de Carga (K): "),
+            dcc.Input(id="input-k", type="number", value=750, className="input-field"),
+        ], className="input-group"),
+
+        html.Div([
+            html.Label("Tiempo Máximo (t):"),
+            dcc.Input(id="input-t", type="number", value=100, className="input-field"),
+        ], className="input-group"),
+
+        html.Button("Generar Gráfica", id='btn-generar', className="btn-generar"),
+    ], className="content left"),
+
+    #contenedor derecho
+    html.Div(children=[
+        html.H2("Gáfica", className="title"),
+
+        dcc.Graph(
+            id = 'grafica-poblacion',
+            style={'height':'350px','width':'100%'},
+        )
+    ], className="content right")
+], className="page-container")
+
+
+## Callbacks ##
+
+@callback(
+    Output('grafica-poblacion', 'figure'),
+    Input('btn-generar', 'n_clicks'),
+    State('input-p0', 'value'),
+    State('input-r', 'value'),
+    State('input-k', 'value'),
+    State('input-t', 'value'),
+    prevent_initial_call=False
+)
+
+def actualizar_grafica(n_clicks, P0, r, K, t_max):
+    
+    fig = func_graficar_ecu_log(P0, r, K, t_max)
+    return fig
+ 
---
+++ utils/funciones.py
@@ -0,0 +1,108 @@
+import numpy as np
+import plotly.graph_objects as go
+
+def func_graficar_ecu_log(P0, r, K, t_max):
+        # Generar los valores el tiempo
+    t = np.linspace(0, t_max, 20)
+ 
+    # Ecuación
+    P = (P0* K * np.exp(r*t)) / ((K-P0) + P0 * np.exp(r*t))
+
+    # Crear Gráfica de la población
+    trace_poblacion = go.Scatter(
+        x = t,
+        y = P,
+        mode='lines+markers',
+        name='Población P(t)',
+        line=dict(
+            color='black',
+            width=2,
+        ),
+        marker=dict(
+            size=6,
+            color='blue', 
+            symbol='circle'
+        ),
+
+        hovertemplate='t: ${x:.2f}<br>P(t): %{y:.2f}<extra></extra>'
+    )
+
+    # Crear gráfica de la capacidad de carga
+    trace_capacidad=go.Scatter(
+        x = [0,t_max],
+        y = [K,K],
+        mode='lines',
+        name='Capacidad de Carga (K)',
+        line=dict(
+            color='red',
+            width=2,
+            dash='dot'
+        ),
+        hovertemplate='K: %{y:.2f}<extra></extra>'
+    )
+
+    fig=go.Figure(data=[trace_poblacion,trace_capacidad])
+
+    fig.update_layout(
+        title=dict(
+            text='<b>Crecimiento Logístico</b>',
+            font=dict(
+                size=20,
+                color='black',
+            ),
+            x=0.5,
+            y=0.95,
+        ),
+        xaxis_title='Tiempo (t)',
+        yaxis_title='Población P(t)',
+        margin=dict(l=40,r=40,t=70,b=40,),
+        paper_bgcolor='lightblue',
+        plot_bgcolor='white',
+        font=dict(
+            family='Output',
+            size=14,
+            color='black',
+        ),
+
+        legend = dict(
+            orientation ='h',
+            yanchor='bottom',
+            y=1.02
+        )
+    )
+
+    fig.update_xaxes(
+        showgrid=True,
+        gridwidth=1,
+        gridcolor='lightpink',
+
+        zeroline=True,
+        zerolinewidth=2,
+        zerolinecolor='red',
+
+        showline=True,
+        linecolor='black',
+        linewidth=2,
+        mirror=True,
+
+        range=[-1,t_max], 
+    )
+
+    fig.update_yaxes(
+        showgrid=True,
+        gridwidth=1,
+        gridcolor='lightpink',
+
+        zeroline=True,
+        zerolinewidth=2,
+        zerolinecolor='red',
+
+        showline=True,
+        linecolor='black',
+        linewidth=2,
+        mirror=True,
+
+        range=[0,(K,K+0.1*K)]
+    )
+
+    return fig
